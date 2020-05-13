7137738f5fea401501ea95b801fc4300a864db6a
vyk1 <victoriabotelho14@gmail.com>
2020-05-12 21:29:50 -0300

    #1. Retorno de Views

---
+++ application/views/home.php
@@ -0,0 +1,395 @@
+<header>
+<div class="container">
+    <div class="slider-container">
+        <div class="intro-text">
+            <div class="intro-lead-in">Welcome To MOOZ Themes!</div>
+            <div class="intro-heading">YOUR FAVORITE SOURCE OF FREE BOOTSTRAP THEMES</div>
+            <a href="#about" class="page-scroll btn btn-xl">Tell Me More</a>
+        </div>
+    </div>
+</div>
+</header>
+<section id="about" class="dark-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="section-title">
+                    <h2>ABOUT</h2>
+                    <p>A creative agency based on Candy Land, ready to boost your business with some beautifull templates. MOOZ Agency is one of the best in town see more you will be amazed.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <!-- about module -->
+            <div class="col-md-3 text-center">
+                <div class="mz-module-about">
+                    <i class="fa fa-briefcase ot-circle"></i>
+                    <h3>Web Development</h3>
+                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+                </div>
+            </div>
+            <!-- end about module -->
+            <!-- about module -->
+            <div class="col-md-3 text-center">
+                <div class="mz-module-about">
+                    <i class="fa fa-photo ot-circle"></i>
+                    <h3>Visualisation</h3>
+                    <p>Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe</p>
+                </div>
+            </div>
+            <!-- end about module -->
+            <!-- about module -->
+            <div class="col-md-3 text-center">
+                <div class="mz-module-about">
+                    <i class="fa fa-camera-retro ot-circle"></i>
+                    <h3>Photography</h3>
+                    <p>Accusamus et iusto odio dignissimos ducimus qui blanditiis</p>
+                </div>
+            </div>
+            <!-- end about module -->
+            <!-- about module -->
+            <div class="col-md-3 text-center">
+                <div class="mz-module-about">
+                    <i class="fa fa-cube ot-circle"></i>
+                    <h3>UI/UX Design</h3>
+                    <p> Itaque earum rerum hic tenetur a sapiente, ut aut reiciendis maiores</p>
+                </div>
+            </div>
+            <!-- end about module -->
+        </div>
+    </div>
+    <!-- /.container -->
+</section>
+<section class="light-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <div class="section-text">
+                    <h3>WE`RE CREATIVE</h3>
+                    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore</p>
+                    <ul>
+                        <li>Responsive Bootstrap Template</li>
+                        <li>Developer Friendly Code</li>
+                        <li>Creative Freedom Experience</li>
+                        <li>MOOZThemes.com Do Things With Love</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-5">
+
+                <div class="owl-portfolio owl-carousel">
+                    <div class="item">
+                        <div class="owl-portfolio-item"><img src="<?php echo base_url();?>public/images/demo/portfolio-7.jpg" class="img-responsive" alt="portfolio"></div>
+                    </div>
+                    <div class="item">
+                        <div class="owl-portfolio-item"><img src="<?php echo base_url();?>public/images/demo/portfolio-8.jpg" class="img-responsive" alt="portfolio"></div>
+                    </div>
+                    <div class="item">
+                        <div class="owl-portfolio-item"><img src="<?php echo base_url();?>public/images/demo/portfolio-9.jpg" class="img-responsive" alt="portfolio"></div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</section>
+
+<section id="portfolio">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="section-title">
+                    <h2>Portfolio</h2>
+                    <p>Our portfolio is the best way to show our work, you can see here a big range of our work. Check them all and you will find what you are looking for.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row row-0-gutter">
+            <!-- start portfolio item -->
+            <div class="col-md-4 col-0-gutter">
+                <div class="ot-portfolio-item">
+                    <figure class="effect-bubba">
+                        <img src="<?php echo base_url();?>public/images/demo/portfolio-1.jpg" alt="img02" class="img-responsive" />
+                        <figcaption>
+                            <h2>Dean & Letter</h2>
+                            <p>Branding, Design</p>
+                            <a href="#" data-toggle="modal" data-target="#Modal-1">View more</a>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+            <!-- end portfolio item -->
+            <!-- start portfolio item -->
+            <div class="col-md-4 col-0-gutter">
+                <div class="ot-portfolio-item">
+                    <figure class="effect-bubba">
+                        <img src="<?php echo base_url();?>public/images/demo/portfolio-2.jpg" alt="img02" class="img-responsive" />
+                        <figcaption>
+                            <h2>Startup Framework</h2>
+                            <p>Branding, Web Design</p>
+                            <a href="#" data-toggle="modal" data-target="#Modal-2">View more</a>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+            <!-- end portfolio item -->
+            <!-- start portfolio item -->
+            <div class="col-md-4 col-0-gutter">
+                <div class="ot-portfolio-item">
+                    <figure class="effect-bubba">
+                        <img src="<?php echo base_url();?>public/images/demo/portfolio-3.jpg" alt="img02" class="img-responsive" />
+                        <figcaption>
+                            <h2>Lamp & Velvet</h2>
+                            <p>Branding, Web Design</p>
+                            <a href="#" data-toggle="modal" data-target="#Modal-3">View more</a>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+            <!-- end portfolio item -->
+        </div>
+        <div class="row row-0-gutter">
+            <!-- start portfolio item -->
+            <div class="col-md-4 col-0-gutter">
+                <div class="ot-portfolio-item">
+                    <figure class="effect-bubba">
+                        <img src="<?php echo base_url();?>public/images/demo/portfolio-4.jpg" alt="img02" class="img-responsive" />
+                        <figcaption>
+                            <h2>Smart Name</h2>
+                            <p>Branding, Design</p>
+                            <a href="#" data-toggle="modal" data-target="#Modal-4">View more</a>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+            <!-- end portfolio item -->
+            <!-- start portfolio item -->
+            <div class="col-md-4 col-0-gutter">
+                <div class="ot-portfolio-item">
+                    <figure class="effect-bubba">
+                        <img src="<?php echo base_url();?>public/images/demo/portfolio-5.jpg" alt="img02" class="img-responsive" />
+                        <figcaption>
+                            <h2>Fast People</h2>
+                            <p>Branding, Web Design</p>
+                            <a href="#" data-toggle="modal" data-target="#Modal-5">View more</a>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+            <!-- end portfolio item -->
+            <!-- start portfolio item -->
+            <div class="col-md-4 col-0-gutter">
+                <div class="ot-portfolio-item">
+                    <figure class="effect-bubba">
+                        <img src="<?php echo base_url();?>public/images/demo/portfolio-6.jpg" alt="img02" class="img-responsive" />
+                        <figcaption>
+                            <h2>Kites & Stars</h2>
+                            <p>Branding, Web Design</p>
+                            <a href="#" data-toggle="modal" data-target="#Modal-2">View more</a>
+                        </figcaption>
+                    </figure>
+                </div>
+            </div>
+            <!-- end portfolio item -->
+        </div>
+    </div><!-- end container -->
+</section>
+
+<section class="dark-bg short-section stats-bar">
+    <div class="container text-center">
+        <div class="row">
+            <div class="col-md-3 mb-sm-30">
+                <div class="counter-item">
+                    <h2 class="stat-number" data-n="9">0</h2>
+                    <h6>awards</h6>
+                </div>
+            </div>
+            <div class="col-md-3 mb-sm-30">
+                <div class="counter-item">
+                    <h2 class="stat-number" data-n="167">0</h2>
+                    <h6>Clients</h6>
+                </div>
+            </div>
+            <div class="col-md-3 mb-sm-30">
+                <div class="counter-item">
+                    <h2 class="stat-number" data-n="6">0</h2>
+                    <h6>Team</h6>
+                </div>
+            </div>
+            <div class="col-md-3 mb-sm-30">
+                <div class="counter-item">
+                    <h2 class="stat-number" data-n="34">0</h2>
+                    <h6>Project</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="section-title">
+                    <h2>Our Partners</h2>
+                    <p>Mida sit una namet, cons uectetur adipiscing adon elit.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="owl-partners owl-carousel">
+                    <div class="item">
+                        <div class="partner-logo"><img src="<?php echo base_url();?>public/images/demo/partners-1.png" alt="partners"></div>
+                    </div>
+                    <div class="item">
+                        <div class="partner-logo"><img src="<?php echo base_url();?>public/images/demo/partners-2.png" alt="partners"></div>
+                    </div>
+                    <div class="item">
+                        <div class="partner-logo"><img src="<?php echo base_url();?>public/images/demo/partners-3.png" alt="partners"></div>
+                    </div>
+                    <div class="item">
+                        <div class="partner-logo"><img src="<?php echo base_url();?>public/images/demo/partners-4.png" alt="partners"></div>
+                    </div>
+                    <div class="item">
+                        <div class="partner-logo"><img src="<?php echo base_url();?>public/images/demo/partners-5.png" alt="partners"></div>
+                    </div>
+                    <div class="item">
+                        <div class="partner-logo"><img src="<?php echo base_url();?>public/images/demo/partners-6.png" alt="partners"></div>
+                    </div>
+                    <div class="item">
+                        <div class="partner-logo"><img src="<?php echo base_url();?>public/images/demo/partners-7.png" alt="partners"></div>
+                    </div>
+                    <div class="item">
+                        <div class="partner-logo"><img src="<?php echo base_url();?>public/images/demo/partners-8.png" alt="partners"></div>
+                    </div>
+                    <div class="item">
+                        <div class="partner-logo"><img src="<?php echo base_url();?>public/images/demo/partners-9.png" alt="partners"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="team" class="light-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="section-title">
+                    <h2>Our Team</h2>
+                    <p>A creative agency based on Candy Land, ready to boost your business with some beautifull templates. MOOZ Agency is one of the best in town see more you will be amazed.</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <!-- team member item -->
+            <div class="col-md-4">
+                <div class="team-item">
+                    <div class="team-image">
+                        <img src="<?php echo base_url();?>public/images/demo/author-2.jpg" class="img-responsive" alt="author">
+                    </div>
+                    <div class="team-text">
+                        <h3>TOM BEKERS</h3>
+                        <div class="team-position">CEO & Web Design</div>
+                        <p>Mida sit una namet, cons uectetur adipiscing adon elit. Aliquam vitae barasa droma.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- end team member item -->
+            <!-- team member item -->
+            <div class="col-md-4">
+                <div class="team-item">
+                    <div class="team-image">
+                        <img src="<?php echo base_url();?>public/images/demo/author-6.jpg" class="img-responsive" alt="author">
+                    </div>
+                    <div class="team-text">
+                        <h3>LINA GOSATA</h3>
+                        <div class="team-position">Photography</div>
+                        <p>Worsa dona namet, cons uectetur dipiscing adon elit. Aliquam vitae fringilla unda mir.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- end team member item -->
+            <!-- team member item -->
+            <div class="col-md-4">
+                <div class="team-item">
+                    <div class="team-image">
+                        <img src="<?php echo base_url();?>public/images/demo/author-4.jpg" class="img-responsive" alt="author">
+                    </div>
+                    <div class="team-text">
+                        <h3>John BEKERS</h3>
+                        <div class="team-position">Marketing</div>
+                        <p>Dolor sit don namet, cons uectetur beriscing adon elit. Aliquam vitae fringilla unda.</p>
+                    </div>
+                </div>
+            </div>
+            <!-- end team member item -->
+        </div>
+    </div>
+</section>
+<section id="contact" class="dark-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 text-center">
+                <div class="section-title">
+                    <h2>Contact Us</h2>
+                    <p>If you have some Questions or need Help! Please Contact Us!<br>We make Cool and Clean Design for your Business</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="section-text">
+                    <h4>Our Business Office</h4>
+                    <p>3422 Street, Barcelona 432, Spain, New Building North, 15th Floor</p>
+                    <p><i class="fa fa-phone"></i> +101 377 655 22125</p>
+                    <p><i class="fa fa-envelope"></i> mail@yourcompany.com</p>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="section-text">
+                    <h4>Business Hours</h4>
+                    <p><i class="fa fa-clock-o"></i> <span class="day">Weekdays:</span><span>9am to 8pm</span></p>
+                    <p><i class="fa fa-clock-o"></i> <span class="day">Saturday:</span><span>9am to 2pm</span></p>
+                    <p><i class="fa fa-clock-o"></i> <span class="day">Sunday:</span><span>Closed</span></p>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <form name="sentMessage" id="contactForm" novalidate="">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input type="text" class="form-control" placeholder="Your Name *" id="name" required="" data-validation-required-message="Please enter your name.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <input type="email" class="form-control" placeholder="Your Email *" id="email" required="" data-validation-required-message="Please enter your email address.">
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-12">
+                            <div class="form-group">
+                                <textarea class="form-control" placeholder="Your Message *" id="message" required="" data-validation-required-message="Please enter a message."></textarea>
+                                <p class="help-block text-danger"></p>
+                            </div>
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-12 text-center">
+                            <div id="success"></div>
+                            <button type="submit" class="btn">Send Message</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<p id="back-top">
+    <a href="#top"><i class="fa fa-angle-up"></i></a>
+</p>
---
+++ application/views/template/scripts.php
@@ -0,0 +1,10 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+		<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+		<script src="<?php echo base_url();?>public/js/bootstrap.min.js"></script>
+		<script src="<?php echo base_url();?>public/js/owl.carousel.min.js"></script>
+		<script src="<?php echo base_url();?>public/js/cbpAnimatedHeader.js"></script>
+		<script src="<?php echo base_url();?>public/js/jquery.appear.js"></script>
+		<script src="<?php echo base_url();?>public/js/SmoothScroll.min.js"></script>
+		<script src="<?php echo base_url();?>public/js/mooz.themes.scripts.js"></script>
+	</body>
+</html>
