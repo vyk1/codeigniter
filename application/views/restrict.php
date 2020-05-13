2d41fa4264155a7fb5cfe657f40179c6a53cd901
vyk1 <victoriabotelho14@gmail.com>
2020-05-13 10:01:32 -0300

    #5 Mensagens ajax no formulário de login

--- application/views/restrict.php
+++ application/views/restrict.php
@@ -1,1 +1,17 @@
-<h1>Área Restrita, pô</h1>
+<section style="min-height: calc(100vh - 83px)" class="light-bg">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-offset-3 col-lg-6 text-center">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <div class="section-title">
+                            <h2>ÁREA RESTRITA</h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container -->
+</section>
--- public/js/util.js
+++ public/js/util.js
@@ -1,21 +1,14 @@
-const BASE_URL = base_url
+const BASE_URL = 'http://localhost:/ignited/'
+// const BASE_URL = base_url
 
 function clearErrors() {
     $('.has-error').removeClass("has-error")
     $('.help-block').html("")
-
 }
 
 function showErrors(error_list) {
     clearErrors()
-    console.log('====================================');
-    console.log(error_list);
-    console.log('====================================');
     $.each(error_list, function (id, message) {
-        console.log('====================================');
-        console.log(id);
-        console.log('====================================');
-        console.log(message);
         $(id).parent().parent().addClass("has-error")
         $(id).parent().siblings(".help-block").html(message)
     })
